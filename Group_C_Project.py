30f9b3b465c9e03d35a91de79fc327852c27a82e
abiwil97 <68333137+abiwil97@users.noreply.github.com>
2020-09-23 21:33:02 +0100

    Update Group_C_Project.py

--- Group_C_Project.py
+++ Group_C_Project.py
@@ -1,39 +1,4 @@
-# Group C - Galyna and Abi
 
-# What was the original plan for the project?
-# Must:
-# Allow user to enter at least one ingredient
-# Search for recipe using that ingredient
-# Display all results
-# Should:
-# Allow user to enter multiple ingredients
-# Display name + at least 2 other details
-# Save to a file displaying all results (formatted)
-# Could:
-# Ask user how long they want to spend making it
-# Ask user what priorities they have Calories, nutritional information
-# Won't:
-# No image
-# Don't include reviews
-
-# What does the project actually do?
-# Asks user to choose a meal type
-# Asks to user enter one or two ingredients
-# Searches for recipes
-# Displays name, cook time, health labels, calories and URL
-# Saves to file
-
-# A brief explanation of an interesting piece of code in the project
-# API - finding out what we couldn't get
-
-# One thing that you learned during the project
-# How to read documentation - encountered new variable type
-
-# A difficult part of the project that you solved
-# Formatting the file
-
-# What would you do if they had more time on the project?
-# Try different API
 
 import requests
 
